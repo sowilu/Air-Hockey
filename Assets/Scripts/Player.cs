8be60c09603e4585d734bafdc5238b63da62f645
ViaceslaVV <sima.simute1999@gmail.com>
2024-10-20 16:25:54 +0300

    Player, scores

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     private Camera mainCamera;
     private Vector3 worldPosition;
     private Rigidbody2D rb;
+    private float halfScreenX;
 
 
     void Start()
@@ -14,12 +15,15 @@ public class Player : MonoBehaviour
         mainCamera = Camera.main;
         Cursor.visible = false;
         rb = GetComponent<Rigidbody2D>();
+
+        halfScreenX = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width / 2, 0, 0)).x;
     }
 
     void Update()
     {
         worldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         worldPosition.z = 0;
+        worldPosition.x = Mathf.Clamp(worldPosition.x, halfScreenX, Mathf.Infinity);
         //transform.position = worldPosition;
     }
 
---
+++ Assets/Scripts/Puk.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puk : MonoBehaviour
+{
+    public AudioClip clip;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clip;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        audioSource.pitch = Random.Range(0.95f, 1.1f);
+        audioSource.Play();
+    }
+}
